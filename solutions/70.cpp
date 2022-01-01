042c059ea085dffccebcf538217180f752d651fb
Ved <dev@vedat.xyz>
2022-01-01 05:10:40 +0300

    Add DP solution to 70

--- solutions/70.cpp
+++ solutions/70.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int climbStairs(int n) {
+        // Solution found after playing around with
+        // modeling the question using bitstrings
+        // Turns out it's just Fibonacci
         vector<int> last2(2, 1);
         
         for (int i = 0; i != n - 1; i++) {
@@ -10,6 +13,17 @@ public:
         }
         
         return last2[1];
+    }
+    
+    // Dynamic programming solution
+    int climbStairs(int n) {
+        vector<int> steps(n + 1, 0);
+        steps[0] = 1;
+        
+        for (int i = 1; i <= n; i++)
+            steps[i] += steps[i - 1] + (i >= 2 ? steps[i - 2] : 0);
+        
+        return steps[n];
     }
 };
 
